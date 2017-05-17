a5cc868fa39b1620fdac573817b3b00c85f79392
terifan <pinjata69@gmail.com>
2017-05-17 16:09:45 +0200

    update

---
+++ src/org/terifan/ui/calendarpane/SimpleCalendarElement.java
@@ -0,0 +1,10 @@
+package org.terifan.ui.calendarpane;
+
+
+public class SimpleCalendarElement extends CalendarElement
+{
+	public SimpleCalendarElement(String aText)
+	{
+		super.setText(aText);
+	}
+}
